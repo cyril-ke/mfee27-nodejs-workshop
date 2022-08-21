4146773ab21099a407128458f5ca4dc0a6f15a23
cyril-ke <cernd206@gmail.com>
2022-08-21 15:30:21 +0800

    [fix] .evn reupload

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-console.log('hello nodejs');
+console.log("hello nodejs");
 
 // 因為 window 是瀏覽器使用的
 //window.location.href = 'http://www.google.com';
--- sum.js
+++ sum.js
@@ -1,10 +1,10 @@
-let sum = function sum(n){
-    let count = 0;
-    for(let i=0; i<=n; i++){
-        count += i;
-    }
-    console.log(count)
-}
-sum(1)
-sum(3)
-sum(10)
+let sum = function sum(n) {
+  let count = 0;
+  for (let i = 0; i <= n; i++) {
+    count += i;
+  }
+  console.log(count);
+};
+sum(1);
+sum(3);
+sum(10);
